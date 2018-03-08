15041cd5883eb7c5af3fbff511190f4b32cbd267
LAPTOP-TomB <tomb97@live.co.uk>
2018-03-08 12:35:16 +0000

    Added second box as the Title

--- popup.js
+++ popup.js
@@ -1,21 +1,24 @@
-document.getElementById("entry").addEventListener("blur", save, false);
-var content = JSON.parse(localStorage.getItem("content")) || [];
+document.getElementById("entry").addEventListener("blur", savetxt, false);
+document.getElementById("titleBox").addEventListener("blur", savetitle, false);
 
+function savetitle() {
+	title = [];
+	var title_text = document.getElementById("titleBox").value;
+	
+	/** saves text entered to the local storage. **/
+	title.push(title_text);
+	localStorage.setItem("title", JSON.stringify(title));	
+}
 
-
-function save() {
+function savetxt() {
 	content = [];
 	var entry_text = document.getElementById("entry").value;
     
 	/** saves text entered to the local storage. **/
 	content.push(entry_text);
 	localStorage.setItem("content", JSON.stringify(content));
-	
-	
-	setTimeout(function() {
-		document.querySelector('.status .value').innerHTML = "";
-	}, 2000); 
 }
 
-/** saved text is immediately visible and ready to be edited **/
-document.getElementById("entry").value = content;
+/** saved text is immediately visible and ready to be edited */
+document.getElementById("titleBox").value = JSON.parse(localStorage.getItem("title")) || [];;
+document.getElementById("entry").value = JSON.parse(localStorage.getItem("content")) || [];
